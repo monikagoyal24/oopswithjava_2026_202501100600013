89c058240bce22c0e7101a0c9b683d76ad3cb764
monikagoyal24 <goel24145@gmail.com>
2026-09-08 12:55:47 +0530

    Added method overloading program

---
+++ Loops.java
@@ -0,0 +1,55 @@
+public class Loops {
+    public static void main(String[] args){
+        // loops
+        int m=1; // 1 t0 10
+        System.out.println(m);
+        m++;
+        // very long code ;
+        // use loops
+
+        // while loops
+        int i = 1;
+        while(i<= 10  ){ // boolean -> true / false 
+            System.out.println(i);
+            i++;
+        }
+        /*
+         while(expression ){
+         //do something ;
+         }
+        */
+
+
+
+        // do while loop
+    /*
+        do{
+        //do something 
+        } while(expression);
+
+    */
+
+      do {
+       System.out.println(i);
+       i++;
+      } while(i <= 10 );
+
+
+
+    //for loop 
+    /*
+    for(initialization ; condition  ; increment ){
+    }
+     */
+
+    for (int a=0 ; a<=10 ; a++){
+        System.out.println(a);
+
+    }
+    // flow of control of for:
+    
+
+
+
+    }
+}
---
+++ Polymorphism.java
@@ -0,0 +1,51 @@
+// public class Polymorphism {
+//     //polymorphism allows a method to behave differently.
+//     // polymorphism (many forms)
+//     /*
+//     it is of two types 
+//      1. compile time (method overloading )->a method inside a class with different signatures is called method overloading 
+//      2. run time (method overriding)_. child class ne ager use redeine kara h to , to run time pe it will be decided , ke run time pe jab obj call hoga to 
+//      kon call hoga ... obv child hoga ..
+
+
+
+//      */
+//     class Addition {
+//         void sum(){
+//             System.out.println("addition");
+//         }
+//         void sum(int o , int b ){
+//             System.out.println("addition " + (a+b));
+//         }
+//         void sum(double a , double b ){
+//             System.out.println("addition : " + (a+b));
+//         }
+//     }
+//     //driver class of overloading 
+//     class Overloading {
+//         psum{}{
+
+//         }
+//     }
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
